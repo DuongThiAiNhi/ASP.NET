6ca7d4d2e98f5171d0fdb222797e1bcfcb72f23a
ainhi <caocao2082004hi@gmail.com>
2025-03-28 14:43:47 +0700

    first commit

---
+++ DuongThiAiNhi_2122110524_ASP.NET/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using DuongThiAiNhi_2122110524_ASP.NET.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DuongThiAiNhi_2122110524_ASP.NET.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ DuongThiAiNhi_2122110524_ASP.NET/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using DuongThiAiNhi_2122110524_ASP.NET.Model;
+using Microsoft.EntityFrameworkCore;
+namespace DuongThiAiNhi_2122110524_ASP.NET.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public Double Price { get; set; }
+    }
+}
