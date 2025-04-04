43a949379320e4388fffe53a854facf3a7245403
ainhi <caocao2082004hi@gmail.com>
2025-04-04 15:40:23 +0700

    BaiTap

---
+++ DuongThiAiNhi_2122110524/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DuongThiAiNhi_2122110524.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DuongThiAiNhi_2122110524.Data  // Đặt namespace chính xác theo cấu trúc thư mục
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; } // Thêm các bảng cần thiết
+        public DbSet<Category> Categories { get; set; } // Thêm các bảng cần thiết
+
+    }
+}
---
+++ DuongThiAiNhi_2122110524/Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace DuongThiAiNhi_2122110524.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
